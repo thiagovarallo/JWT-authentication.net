8b75a080eb548e659894886fe977008ababb15dd
Thiago Varallo <thiagovaralllo@gmail.com>
2023-11-08 20:28:43 -0300

    Create Auth Controller and Register Action Method

---
+++ JWT authentication/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using JWT_authentication.Data.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public UserManager<IdentityUser> UserManager { get; }
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto dto )
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = dto.Username,
+                Email = dto.Username
+            };
+
+            var identityResult = await UserManager.CreateAsync(identityUser, dto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // add roles to this User
+                if (dto.Roles != null && dto.Roles.Any() ) 
+                {
+                   identityResult = await UserManager.AddToRolesAsync(identityUser, dto.Roles);
+                
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered! Please login :)");
+                    }
+                }
+            }
+
+            return BadRequest("Something went wrong");
+        }
+    }
+}
