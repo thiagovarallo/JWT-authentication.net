a289ad2610e14c04d7f82d2ae0dc7c1d9123ad1e
Thiago Varallo <thiagovaralllo@gmail.com>
2023-11-09 20:59:23 -0300

    Create JWT Token

---
+++ JWT authentication/Repository/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JWT_authentication.Repository
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
